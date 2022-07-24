7cef8b3258f4d4684b90eac9dd920701c92f463c
liuzhiqi <j419230944@hotmail.com>
2022-07-24 16:44:55 +0800

    add configuration.

---
+++ lzq-blog/src/main/java/com/lzq/LZQBlogApplication.java
@@ -0,0 +1,11 @@
+package com.lzq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LZQBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LZQBlogApplication.class, args);
+    }
+}
